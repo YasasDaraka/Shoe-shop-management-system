7447db83b95ea9d9b984e23418b2c66c68d7ff48
YasasDaraka <darakayasas@gmail.com>
2024-04-20 09:06:12 +0530

    first commit

---
+++ BackEnd/src/main/java/lk/ijse/gdse66/helloshoes/HelloshoesApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.helloshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloshoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloshoesApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/lk/ijse/gdse66/helloshoes/HelloshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.helloshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
