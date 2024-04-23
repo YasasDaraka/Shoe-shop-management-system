8e5ad689a31622d39529b31acd66093985859f5e
YasasDaraka <darakayasas@gmail.com>
2024-04-23 16:04:31 +0530

    Frontend Update

--- FrontEnd/controller/indexController.js
+++ FrontEnd/controller/indexController.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+    $('#order-thead').css({
+        'width': '100%',
+        'display': 'flex'
+    });
+    $('#order-thead>th').css({
+        'flex': '1',
+        'max-width': 'calc(100%/5*1)'
+    })
+});
 
