65f3a1e026c8e45dec9e63609e22d75ee5fd2753
YasasDaraka <darakayasas@gmail.com>
2024-04-25 23:43:39 +0530

    Backend Update

--- FrontEnd/controller/orderController.js
+++ FrontEnd/controller/orderController.js
@@ -4,3 +4,8 @@ $("#paymentCard").click(function () {
     paymentPage.css('display','block');
     cardPage.css('display','block');
 });
+$("#cancel").click(function () {
+    cardPage.css('display','none');
+    adminPage.css('display','block');
+    paymentPage.css('display','block');
+});
